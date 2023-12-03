e4a8713539fba35707e0ce84a986f5d2b471685c
meghan16 <meghan16@uw.edu>
2023-12-03 02:16:32 -0800

    added pci data

--- INRIX_Hack_Client_Server_Demo-main/INRIX_Hack_Client_Server_Demo-main/main.py
+++ INRIX_Hack_Client_Server_Demo-main/INRIX_Hack_Client_Server_Demo-main/main.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template, jsonify, request
 from app.utils.auth_utils import get_token
 from flask_cors import CORS
 import requests
+import pandas as pd
+from sodapy import Socrata
 
 # Create the Flask app with the template folder specified that will contain your index.html and static folder which will contain your JavaScript files
 app = Flask(__name__, template_folder='app/templates', static_url_path='/static')
@@ -99,6 +103,67 @@ def get_drawn_routes(routeID):
     except (KeyError, ValueError) as e:
         return f'Error parsing JSON: {e}', None
 
+
+@app.route('/getPCI', methods=['GET'])
+def get_pcisf():
+    # This makes the call to the get_token function in the auth_utils.py file
+    # client = Socrata("data.sfgov.org", None)
+    # url = "https://data.sfgov.org/resource/5aye-4rtt.json"
+
+    # response = client.get(url, limit=2000)
+    # #    response.raise_for_status()  # Raise HTTPError for bad responses
+
+    # #     data = response.json()
+    # # results = client.get("Saye-4rtt", limit=2000)
+
+    # results_df = pd.DataFrame.from_records(response)
+    # data = results_df
+
+
+    api_url = "https://data.sfgov.org/resource/5aye-4rtt.json"
+
+
+    # Include your app token in the headers (replace 'YOUR_APP_TOKEN' with your actual app token)
+    headers = {
+        "X-App-Token": "mGJmcWqEn40OJmCpB9UY20tEi"
+    }
+
+
+    # Make a GET request to the API
+    response = requests.get(api_url, headers=headers)
+
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+
+        # Example: Print the first 5 records
+        for record in data[:5]:
+            print(record)
+
+
+        # Example: Filter records with PCI_Score greater than 80
+        filtered_data = [record for record in data if int(record.get("pci_score", 0)) > 80]
+        print("\nFiltered Data (PCI_Score > 80):")
+        for record in filtered_data[:5]:
+            print(record)
+        return data, response.status_code
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return 
+
+
+    return "hi"
+    # response, status_code = get_token()
+    # # If the request is successful, return the token
+    # if status_code == 200:
+    #     api_token = response
+    #     return jsonify({'message': api_token})
+    # #If the request fails, return the error message
+    # else:
+    #     return jsonify({'message': response})
 
 
 
